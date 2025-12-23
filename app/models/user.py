b57900c15c23726ce97b4eba98bd290470f09b62
pushkinpushkin <pushkinsergey.voronezh@yandex.ru>
2025-12-24 01:43:33 +0300

    Handle created_at in user model

--- app/models/user.py
+++ app/models/user.py
@@ -1,4 +1,7 @@
 from dataclasses import dataclass
+from datetime import datetime
+from typing import Optional
+
 
 @dataclass
 class User:
@@ -7,6 +10,7 @@ class User:
     password_hash: str
     role: str
     full_name: str
+    created_at: Optional[datetime] = None
 
     @property
     def is_authenticated(self):
